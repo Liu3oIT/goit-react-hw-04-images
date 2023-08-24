d80d57568ac6c7a299287e8ec44a458e7322024c
Liu3oIT <liutyivova995@gmail.com>
2023-08-24 21:15:43 +0300

    fix

--- src/components/Apiserver/Api.jsx
+++ src/components/Apiserver/Api.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import GalleryItem from '../GalleryItem/axiorequest';
 import Form from '../FormForSubmit/form';
 
@@ -9,7 +8,9 @@ const Api = () => {
   const [page, setPage] = useState(1);
   const onFormSearch = query => {
     setSearchQuery(prevState => {
-      if (prevState.searchQuery === searchQuery) return prevState;
+      if (prevState.searchQuery === searchQuery) {
+        return prevState;
+      }
       setSearchQuery(query);
       setPage(1);
       setArticles([]);
